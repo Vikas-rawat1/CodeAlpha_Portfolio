342c69fe618ffbed9bc98c7c7ec3ae2ae511a11a
vikas <rawatvikas689@gmail.com>
2024-02-03 23:29:21 +0530

    container config

---
+++ js/typedata.js
@@ -0,0 +1,11 @@
+  const typingdata = new Typed(".role", {
+    strings: [
+      "Web Developer",
+      "Coder",
+      "Frontend Developer ",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+  });
